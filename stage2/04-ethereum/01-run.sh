0f2b32d8ae17d7af17554ec092451242bcd4b9ad
diglos <diego.losadaf@gmail.com>
2016-12-08 19:21:47 +0100

    modified run.sh

--- stage2/04-ethereum/01-run.sh
+++ stage2/04-ethereum/01-run.sh
@@ -1,13 +1,13 @@
 #!/bin/bash -e
 
-install -m 755 files/parity-rasp_1.4.4-0_armhf.deb		${ROOTFS_DIR}/root/
-install -m 755 files/geth-rasp_1.5.2-0_armhf.deb                ${ROOTFS_DIR}/root/
+install -m 755 files/parity-rasp_1.4.6-0_armhf.deb		${ROOTFS_DIR}/root/
+install -m 755 files/geth-rasp_1.5.4-1_armhf.deb                ${ROOTFS_DIR}/root/
 install -m 755 files/20auto-upgrades				${ROOTFS_DIR}/etc/apt/apt.conf.d/
 install -m 644 files/10apt					${ROOTFS_DIR}/etc/apt/apt.conf.d/
 
 on_chroot sh -e - <<EOF
-dpkg -i /root/parity-rasp_1.4.4-0_armhf.deb
-rm -f /root/parity-rasp_1.4.4-0_armhf.deb
-dpkg -i /root/geth-rasp_1.5.2-0_armhf.deb
-rm -f /root/geth-rasp_1.5.2-0_armhf.deb
+dpkg -i /root/parity-rasp_1.4.6-0_armhf.deb
+rm -f /root/parity-rasp_1.4.6-0_armhf.deb
+dpkg -i /root/geth-rasp_1.5.4-1_armhf.deb
+rm -f /root/geth-rasp_1.5.4-1_armhf.deb
 EOF

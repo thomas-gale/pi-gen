0405e1762c6694c51599c00682ecd429dbd78cb0
diglos <diego.losadaf@gmail.com>
2017-02-19 00:28:07 +0100

    New release

--- stage2/04-ethereum/01-run.sh
+++ stage2/04-ethereum/01-run.sh
@@ -1,5 +1,6 @@
 #!/bin/bash -e
 
+install -m 755 files/libssl1.1_1.1.0d-2_armhf.deb		${ROOTFS_DIR}/root/
 install -m 755 files/parity-rasp_1.5.2-0_armhf.deb		${ROOTFS_DIR}/root/
 install -m 755 files/geth-rasp_1.5.9-0_armhf.deb                ${ROOTFS_DIR}/root/
 install -m 755 files/ipfs_0.4.5-0_armhf.deb			${ROOTFS_DIR}/root/
@@ -7,10 +8,13 @@ install -m 755 files/20auto-upgrades				${ROOTFS_DIR}/etc/apt/apt.conf.d/
 install -m 644 files/10apt					${ROOTFS_DIR}/etc/apt/apt.conf.d/
 
 on_chroot sh -e - <<EOF
-dpkg -i /root/geth-rasp_1.5.9-0_armhf.deb
-rm -f /root/geth-rasp_1.5.9-0_armhf.deb
+dpkg -i /root/libssl1.1_1.1.0d-2_armhf.deb
+rm -f /root/libssl1.1_1.1.0d-2_armhf.deb
 dpkg -i /root/ipfs_0.4.5-0_armhf.deb
 rm -f /root/ipfs_0.4.5-0_armhf.deb
+dpkg -i /root/geth-rasp_1.5.9-0_armhf.deb
+rm -f /root/geth-rasp_1.5.9-0_armhf.deb
 dpkg -i /root/parity-rasp_1.5.2-0_armhf.deb
 rm -f /root/parity-rasp_1.5.2-0_armhf.deb
 EOF
+

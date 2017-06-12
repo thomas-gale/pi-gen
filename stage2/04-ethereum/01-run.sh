f92d29fe6bf133d07c483ebfb69a93d546df73e8
diglos <diego.losadaf@gmail.com>
2017-06-12 21:40:57 +0200

    updated run script

--- stage2/04-ethereum/01-run.sh
+++ stage2/04-ethereum/01-run.sh
@@ -1,20 +1,17 @@
 #!/bin/bash -e
 
-install -m 755 files/libssl1.1_1.1.0d-2_armhf.deb		${ROOTFS_DIR}/root/
-install -m 755 files/parity-rasp_1.6.6-0_armhf.deb		${ROOTFS_DIR}/root/
-install -m 755 files/geth-rasp_1.6.0-0_armhf.deb                ${ROOTFS_DIR}/root/
-install -m 755 files/ipfs_0.4.8-0_armhf.deb			${ROOTFS_DIR}/root/
+install -m 755 files/parity-rasp_1.6.8-0_armhf.deb		${ROOTFS_DIR}/root/
+install -m 755 files/geth-rasp_1.6.5-0_armhf.deb                ${ROOTFS_DIR}/root/
+install -m 755 files/ipfs_0.4.9-0_armhf.deb			${ROOTFS_DIR}/root/
 install -m 755 files/20auto-upgrades				${ROOTFS_DIR}/etc/apt/apt.conf.d/
 install -m 644 files/10apt					${ROOTFS_DIR}/etc/apt/apt.conf.d/
 
 on_chroot sh -e - <<EOF
-dpkg -i /root/libssl1.1_1.1.0d-2_armhf.deb
-rm -f /root/libssl1.1_1.1.0d-2_armhf.deb
-dpkg -i /root/ipfs_0.4.8-0_armhf.deb
-rm -f /root/ipfs_0.4.8-0_armhf.deb
-dpkg -i /root/geth-rasp_1.6.0-0_armhf.deb
-rm -f /root/geth-rasp_1.6.0-0_armhf.deb
-dpkg -i /root/parity-rasp_1.6.6-0_armhf.deb
-rm -f /root/parity-rasp_1.6.6-0_armhf.deb
+dpkg -i /root/ipfs_0.4.9-0_armhf.deb
+rm -f /root/ipfs_0.4.9-0_armhf.deb
+dpkg -i /root/geth-rasp_1.6.5-0_armhf.deb
+rm -f /root/geth-rasp_1.6.5-0_armhf.deb
+dpkg -i /root/parity-rasp_1.6.8-0_armhf.deb
+rm -f /root/parity-rasp_1.6.8-0_armhf.deb
 EOF
 

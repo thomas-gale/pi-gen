986f973baffdbac0d5a9697fe83214fb0ec6546e
diglos <diego.losadaf@gmail.com>
2017-10-10 12:38:06 +0200

    minor filename fix

--- stage2/04-ethereum/01-run.sh
+++ stage2/04-ethereum/01-run.sh
@@ -1,7 +1,7 @@
 #!/bin/bash -e
 
 install -m 755 files/parity-rasp_1.7.3-0_armhf.deb		${ROOTFS_DIR}/root/
-install -m 755 files/geth-rasp_1.7.2-0_armhf.deb                ${ROOTFS_DIR}/root/
+install -m 755 files/geth-rasp_1.7.1-0_armhf.deb                ${ROOTFS_DIR}/root/
 install -m 755 files/ipfs_0.4.11-0_armhf.deb			${ROOTFS_DIR}/root/
 install -m 755 files/20auto-upgrades				${ROOTFS_DIR}/etc/apt/apt.conf.d/
 install -m 644 files/10apt					${ROOTFS_DIR}/etc/apt/apt.conf.d/
@@ -9,8 +9,8 @@ install -m 644 files/10apt					${ROOTFS_DIR}/etc/apt/apt.conf.d/
 on_chroot sh -e - <<EOF
 dpkg -i /root/ipfs_0.4.11-0_armhf.deb
 rm -f /root/ipfs_0.4.11-0_armhf.deb
-dpkg -i /root/geth-rasp_1.7.2-0_armhf.deb
-rm -f /root/geth-rasp_1.7.2-0_armhf.deb
+dpkg -i /root/geth-rasp_1.7.1-0_armhf.deb
+rm -f /root/geth-rasp_1.7.1-0_armhf.deb
 dpkg -i /root/parity-rasp_1.7.3-0_armhf.deb
 rm -f /root/parity-rasp_1.7.3-0_armhf.deb
 EOF

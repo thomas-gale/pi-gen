22d4e6b54257b85b046f9f9f57e1d3da1c92dc77
diglos <diego.losadaf@gmail.com>
2017-10-13 11:03:32 +0200

    parity upgrade

--- stage2/04-ethereum/01-run.sh
+++ stage2/04-ethereum/01-run.sh
@@ -1,6 +1,6 @@
 #!/bin/bash -e
 
-install -m 755 files/parity-rasp_1.7.3-0_armhf.deb		${ROOTFS_DIR}/root/
+install -m 755 files/parity-rasp_1.7.5-0_armhf.deb		${ROOTFS_DIR}/root/
 install -m 755 files/geth-rasp_1.7.1-0_armhf.deb                ${ROOTFS_DIR}/root/
 install -m 755 files/ipfs_0.4.11-0_armhf.deb			${ROOTFS_DIR}/root/
 install -m 755 files/20auto-upgrades				${ROOTFS_DIR}/etc/apt/apt.conf.d/
@@ -11,7 +11,7 @@ dpkg -i /root/ipfs_0.4.11-0_armhf.deb
 rm -f /root/ipfs_0.4.11-0_armhf.deb
 dpkg -i /root/geth-rasp_1.7.1-0_armhf.deb
 rm -f /root/geth-rasp_1.7.1-0_armhf.deb
-dpkg -i /root/parity-rasp_1.7.3-0_armhf.deb
-rm -f /root/parity-rasp_1.7.3-0_armhf.deb
+dpkg -i /root/parity-rasp_1.7.5-0_armhf.deb
+rm -f /root/parity-rasp_1.7.5-0_armhf.deb
 EOF
 

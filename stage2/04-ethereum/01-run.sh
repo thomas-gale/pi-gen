5adee905fcbe788777f1bc1f78698b9bec2087e3
diglos <diego.losadaf@gmail.com>
2019-03-01 14:33:51 +0000

    added raiden

--- stage2/04-ethereum/01-run.sh
+++ stage2/04-ethereum/01-run.sh
@@ -1,9 +1,12 @@
 #!/bin/bash -e
 
 install -m 755 files/geth_1.8.23-0_armhf.deb                ${ROOTFS_DIR}/root/
+install -m 755 files/raiden_0.100.2-0_armhf.deb		    ${ROOTFS_DIR}/root/
 
-on_chroot sh -e - <<EOF
+on_chroot <<EOF
 dpkg -i /root/geth_1.8.23-0_armhf.deb
 rm -f /root/geth_1.8.23-0_armhf.deb
+dpkg -i /root/raiden_0.100.2-0_armhf.deb
+rm -f /root/raiden_0.100.2-0_armhf.deb
 EOF
 

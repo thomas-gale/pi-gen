7f07d7c70ab44aa559cc8840a66cbadc04c93aa6
vagrant <vagrant@vagrant.vm>
2019-03-06 22:49:36 +0000

    add parity and bug fixes

--- stage2/04-ethereum/01-run.sh
+++ stage2/04-ethereum/01-run.sh
@@ -2,10 +2,13 @@
 
 install -m 755 files/geth_1.8.23-0_armhf.deb                ${ROOTFS_DIR}/root/
 install -m 755 files/raiden_0.100.2-0_armhf.deb		    ${ROOTFS_DIR}/root/
+install -m 755 files/parity_2.3.5-0_armhf.deb		    ${ROOTFS_DIR}/root/
 
 on_chroot <<EOF
 dpkg -i /root/geth_1.8.23-0_armhf.deb
 rm -f /root/geth_1.8.23-0_armhf.deb
+dpkg -i /root/parity_2.3.5-0_armhf.deb
+rm -f /root/parity_2.3.5-0_armhf.deb
 dpkg -i /root/raiden_0.100.2-0_armhf.deb
 rm -f /root/raiden_0.100.2-0_armhf.deb
 EOF

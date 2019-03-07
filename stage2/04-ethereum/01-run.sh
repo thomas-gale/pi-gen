2765541090c640c77c8b49bfcd36ec7cf9a3487b
diglos <diego.losadaf@gmail.com>
2019-03-07 11:00:23 +0000

    added status node

--- stage2/04-ethereum/01-run.sh
+++ stage2/04-ethereum/01-run.sh
@@ -3,6 +3,7 @@
 install -m 755 files/geth_1.8.23-0_armhf.deb                ${ROOTFS_DIR}/root/
 install -m 755 files/raiden_0.100.2-0_armhf.deb		    ${ROOTFS_DIR}/root/
 install -m 755 files/parity_2.3.5-0_armhf.deb		    ${ROOTFS_DIR}/root/
+install -m 755 files/status.im-node-0.23.0-beta8_armhf.deb  ${ROOTFS_DIR}/root/
 
 on_chroot <<EOF
 dpkg -i /root/geth_1.8.23-0_armhf.deb
@@ -11,5 +12,7 @@ dpkg -i /root/parity_2.3.5-0_armhf.deb
 rm -f /root/parity_2.3.5-0_armhf.deb
 dpkg -i /root/raiden_0.100.2-0_armhf.deb
 rm -f /root/raiden_0.100.2-0_armhf.deb
+dpkg -i /root/status.im-node-0.23.0-beta8_armhf.deb
+rm -f /root/status.im-node-0.23.0-beta8_armhf.deb
 EOF
 

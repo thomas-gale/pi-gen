52e23b871a38b4020f2f5a5408c78fae6e9fd05c
diglos <diego.losadaf@gmail.com>
2019-03-08 16:25:00 +0000

    added ipfs and update script

--- stage2/04-ethereum/01-run.sh
+++ stage2/04-ethereum/01-run.sh
@@ -4,6 +4,8 @@ install -m 755 files/geth_1.8.23-0_armhf.deb                ${ROOTFS_DIR}/root/
 install -m 755 files/raiden_0.100.2-0_armhf.deb		    ${ROOTFS_DIR}/root/
 install -m 755 files/parity_2.3.5-0_armhf.deb		    ${ROOTFS_DIR}/root/
 install -m 755 files/status.im-node-0.23.0-beta8_armhf.deb  ${ROOTFS_DIR}/root/
+install -m 755 files/ipfs_0.4.19-0_armhf.deb		    ${ROOTFS_DIR}/root/
+install -m 755 files/update-ethereum			    ${ROOTFS_DIR}/usr/local/bin
 
 on_chroot <<EOF
 dpkg -i /root/geth_1.8.23-0_armhf.deb
@@ -14,5 +16,7 @@ dpkg -i /root/raiden_0.100.2-0_armhf.deb
 rm -f /root/raiden_0.100.2-0_armhf.deb
 dpkg -i /root/status.im-node-0.23.0-beta8_armhf.deb
 rm -f /root/status.im-node-0.23.0-beta8_armhf.deb
+dpkg -i /root/ipfs_0.4.19-0_armhf.deb
+rm -f /root/ipfs_0.4.19-0_armhf.deb
 EOF
 

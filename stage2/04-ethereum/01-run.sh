7d0a012f0b5c101ec45a9261931b4fc36dd79c3d
vagrant <vagrant@vagrant.vm>
2020-03-12 12:12:40 +0000

    enabled armbian zram

--- stage2/04-ethereum/01-run.sh
+++ stage2/04-ethereum/01-run.sh
@@ -4,8 +4,9 @@ install -m 644 files/ethonarm.list			    ${ROOTFS_DIR}/etc/apt/sources.list.d/
 install -m 755 files/init_resize.sh			    ${ROOTFS_DIR}/usr/lib/raspi-config	
 install -m 644 files/dphys-swapfile			    ${ROOTFS_DIR}/etc/
 install -m 644 files/zram-default/armbian-zram-config	    ${ROOTFS_DIR}/etc/default
-install -D -m 755 files/zram-lib/armbian-zram-config	    ${ROOTFS_DIR}/usr/lib/armbian/
-install -m 644 files/armbian-zram-config.service	    ${ROOTFS_DIR}etc/systemd/system/
+install -D -m 755 files/zram-lib/armbian-zram-config	    ${ROOTFS_DIR}/usr/lib/armbian/armbian-zram-config
+install -m 644 files/armbian-ramlog			    ${ROOTFS_DIR}/etc/default
+install -m 644 files/armbian-zram-config.service	    ${ROOTFS_DIR}/etc/systemd/system/
 
 cat <<EOF >> ${ROOTFS_DIR}/etc/bash.bashrc
 alias update-ethereum='
@@ -21,5 +22,7 @@ apt-get update && apt-get install geth
 apt-get install swarm parity nethermind ipfs raiden status.im-node vipnode
 # Force password change on Ethereum account
 #chage -d 0 ethereum
+# enable zram-service
+systemctl enable armbian-zram-config
 EOF
 
